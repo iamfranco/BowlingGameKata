42d72448d3acf12038df6ddf48668b42106fc68c
iamfranco <kinfaifrancochan@gmail.com>
2022-05-21 14:36:38 +0100

    add test cases and implement methods

---
+++ BowlingGame.Tests/UnitTest1.cs
@@ -0,0 +1,46 @@
+using FluentAssertions;
+
+namespace BowlingGame.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CalculateScore_Should_Return_300_If_All_Rolls_Are_Xs()
+        {
+            Bowling.CalculateScore("X X X X X X X X X X X X").Should().Be(300);
+        }
+
+        [Test]
+        public void CalculateScore_Should_Return_90_If_All_Rolls_Are_9s()
+        {
+            Bowling.CalculateScore("9- 9- 9- 9- 9- 9- 9- 9- 9- 9-").Should().Be(90);
+        }
+
+        [Test]
+        public void CalculateScore_Should_Return_150_If_All_Rolls_Are_5s()
+        {
+            Bowling.CalculateScore("5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5").Should().Be(150);
+        }
+
+        [Test]
+        public void CalculateScore_Should_Return_Correct_Score_For_Normal_Rolls()
+        {
+            Bowling.CalculateScore("-- -- -- -- -- -- -- -- -- --").Should().Be(0);
+            Bowling.CalculateScore("11 11 11 11 11 11 11 11 11 11").Should().Be(20);
+            Bowling.CalculateScore("22 22 22 22 22 22 22 22 22 22").Should().Be(40);
+        }
+
+        [Test]
+        public void CalculateScore_Should_Return_Correct_Score_For_Strikes_And_Spares_Mixes()
+        {
+            Bowling.CalculateScore("-/ -- -/ 1- -/ -/ 9- -- -- --").Should().Be(60);
+            Bowling.CalculateScore("X 11 11 X 22 21 X X 9- --").Should().Be(94);
+            Bowling.CalculateScore("5/ X 5- X 3/ 8/ X 1- -- X 5/").Should().Be(130);
+        }
+    }
+}
---
+++ BowlingGame/Bowling.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGame
+{
+    public class Bowling
+    {
+        private enum FrameStatusEnum
+        {
+            strike,
+            spare,
+            normal
+        }
+
+        public static int CalculateScore(string gamePins)
+        {
+            int score = 0;
+
+            int[] numOfPinsDownAtRoll = GetNumOfPinsDownForEveryRoll(gamePins);
+            FrameStatusEnum[] firstTenFrameStatuses = GetFirstTenFrameStatuses(gamePins);
+
+            int rollCount = 0;
+            foreach (FrameStatusEnum frameStatus in firstTenFrameStatuses)
+            {
+                int frameScore;
+                int bonusScore;
+
+                if (frameStatus == FrameStatusEnum.strike)
+                {
+                    frameScore = numOfPinsDownAtRoll[rollCount];
+                    bonusScore = numOfPinsDownAtRoll[rollCount + 1] + numOfPinsDownAtRoll[rollCount + 2];
+                    rollCount++;
+                }
+                else if (frameStatus == FrameStatusEnum.spare)
+                {
+                    frameScore = numOfPinsDownAtRoll[rollCount] + numOfPinsDownAtRoll[rollCount + 1];
+                    bonusScore = numOfPinsDownAtRoll[rollCount + 2];
+                    rollCount += 2;
+                }
+                else
+                {
+                    frameScore = numOfPinsDownAtRoll[rollCount] + numOfPinsDownAtRoll[rollCount + 1];
+                    bonusScore = 0;
+                    rollCount += 2;
+                }
+                score += frameScore + bonusScore;
+            }
+
+            return score;
+        }
+
+        private static int[] GetNumOfPinsDownForEveryRoll(string gamePins)
+        {
+            List<int> rollPins = new();
+            foreach (char c in gamePins)
+            {
+                if (c == 'X')
+                    rollPins.Add(10);
+                else if (c == '/')
+                    rollPins.Add(10 - rollPins.Last());
+                else if (c == '-')
+                    rollPins.Add(0);
+                else if (c != ' ')
+                    rollPins.Add(int.Parse(c.ToString()));
+            }
+
+            return rollPins.ToArray();
+        }
+
+        private static FrameStatusEnum[] GetFirstTenFrameStatuses(string gamePins)
+        {
+            return gamePins.Split(' ').ToList().GetRange(0, 10).Select(item =>
+            {
+                if (item == "X")
+                    return FrameStatusEnum.strike;
+
+                if (item.Contains('/'))
+                    return FrameStatusEnum.spare;
+
+                return FrameStatusEnum.normal;
+            }).ToArray();
+        }
+    }
+}
