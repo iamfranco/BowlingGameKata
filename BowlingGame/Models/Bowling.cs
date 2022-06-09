2766a9bb21510c3a8884d529c9ea50463cfc9cee
iamfranco <kinfaifrancochan@gmail.com>
2022-06-09 10:25:14 +0100

    modify Bowling.AddRoll so it appends a new frame after frame completion rather than before roll, modify with unit test

--- BowlingGame/Models/Bowling.cs
+++ BowlingGame/Models/Bowling.cs
@@ -13,7 +13,7 @@ public class Bowling
     {
         _scoreCalculator = new();
         _bonusRolls = 0;
-        Frames = new();
+        Frames = new() { new Frame() };
         TotalScore = 0;
         IsComplete = false;
     }
@@ -23,15 +23,13 @@ public class Bowling
         if (IsComplete)
             throw new Exception("Game is completed, cannot roll more.");
 
-        if (!Frames.Any() || Frames.Last().Status.IsComplete())
-            Frames.Add(new());
-
         Frame currentFrame = Frames.Last();
 
         currentFrame.AddRoll(pinsDown);
 
         TotalScore += _scoreCalculator.CalculateCurrentRollScore(currentFrame, Frames.Count - 1);
         UpdateBonusRollsAndIsComplete(currentFrame);
+        PrepareNextFrame();
     }
 
     private void UpdateBonusRollsAndIsComplete(Frame currentFrame)
@@ -52,5 +50,11 @@ public class Bowling
 
         if (Frames.Count > STANDARD_MAX_FRAMES && _bonusRolls == 0)
             IsComplete = true;
+    }
+
+    private void PrepareNextFrame()
+    {
+        if (Frames.Last().Status.IsComplete())
+            Frames.Add(new());
     }
 }
